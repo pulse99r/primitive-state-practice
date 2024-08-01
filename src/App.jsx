241b763957ed564dd4b26a19f257b17cb1e77c2e
Raymond <raymondjohnson@pursuit.org>
2024-08-01 13:22:03 -0400

    init: initial commit - exercise complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Box from "./components/Box";
 
 function App() {
-  return <div className="app"></div>;
+  return <div className="app">
+    <Box/>
+    <Box/>
+    <Box/>
+    <Box/>
+    <Box/>
+  </div>;
 }
 
 export default App;
---
+++ src/components/Box.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import './Box.css'
+
+const Box = () => {
+  const [color, setColor] = useState('true');
+  const changeColor = () => {
+    setColor(!color)
+  }
+  return (
+    <div id="box" className={color ? "red": "blue"} onClick={changeColor}>
+      {color ? (
+        <p>Red</p>
+      )  : (
+        <span>
+          <p>Blue - hover over box for red; </p>
+          <p>Click the change to red when not hovering</p>
+
+        </span>
+      )}
+    </div>
+  );
+}
+
+export default Box;
